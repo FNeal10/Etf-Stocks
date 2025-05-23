c98527f09301b2d4e05550fb95053a3970368222
FNeal10 <altares.fneal@gmail.com>
2025-05-23 15:47:54 -0400

    Friday

---
+++ src/data/blob_utils.py
@@ -0,0 +1,33 @@
+
+from io import BytesIO
+import pandas as pd
+
+
+def get_urls(container):
+    blob_name = "raw/market-urls.csv"
+    blob_client = container.get_blob_client(blob=blob_name)
+
+    data = blob_client.download_blob().readall()
+    df = pd.read_csv(BytesIO(data))
+
+    return df
+
+def append_prices(container, ticker:str, tickerType:str, prices:list):
+    print(f"Appending prices for {ticker} of type {tickerType}")
+    blob_name = f"bronze/{ticker}.csv"
+    blob_client = container.get_blob_client(blob=blob_name)
+
+    data = blob_client.download_blob()
+    blob_data = data.readall()
+    df = pd.read_csv(BytesIO(blob_data))
+
+    column_names = ["Date", "Open", "High", "Low", "Close", "Volume"]
+    prices[0] = str(prices[0]) 
+
+    new_df = pd.DataFrame([prices], columns=column_names)
+    df = pd.concat([new_df, df], ignore_index=True)
+   
+    updated_blob_data = df.to_csv(index=False).encode('utf-8')
+    blob_client.upload_blob(updated_blob_data, overwrite=True)
+
+    return 1
