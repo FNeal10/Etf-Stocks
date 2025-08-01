8581c594fea5b6662f3041545a69d8a7cf556272
FNeal10 <altares.fneal@gmail.com>
2025-08-01 17:50:02 -0400

    Friday

--- src/my_app/transform_to_silver.py
+++ src/my_app/transform_to_silver.py
@@ -7,16 +7,19 @@ from azure.storage.blob import BlobServiceClient
 
 from src.utils.helper import format_to_decimal, append_to_log
 
-def clean_and_add_features(df: pd.DataFrame, source: str):
-
-    df.insert(0, "Source", source)
+def clean_data(df):
     df["Volume"] = df["Volume"].str.replace(',','').replace("'","").replace(".","").astype(int)
     df["Date"] = pd.to_datetime(df["Date"], format='%m/%d/%Y')
     
-    df["Open"] = df["Open"].apply(format_to_decimal).astype(float)
+    df["Open"] = 678 #df["Open"].apply(format_to_decimal).astype(float)
     df["High"] = df["High"].apply(format_to_decimal).astype(float)
     df["Close"] = df["Close"].apply(format_to_decimal).astype(float)
     df["Low"] = df["Low"].apply(format_to_decimal).astype(float)
+    return df
+
+
+def add_features(df: pd.DataFrame, source: str):
+    df.insert(0, "Source", source)    
 
     # Dates
     df["Month"] = df["Date"].dt.month
@@ -24,8 +27,8 @@ def clean_and_add_features(df: pd.DataFrame, source: str):
     df["Year"] = df["Date"].dt.year
     df["DayOfWeek"] = df["Date"].dt.dayofweek
 
-    df["MA_5"] = df["Close"].rolling(5).std().apply(format_to_decimal)
-    df["MA_20"] = df["Close"].rolling(20).std().apply(format_to_decimal)
+    df["MA_5"] = df["Close"].rolling(5).mean().apply(format_to_decimal)
+    df["MA_20"] = df["Close"].rolling(20).mean().apply(format_to_decimal)
     df["EMA_5"] = df["Close"].ewm(span=5, adjust=False).mean().apply(format_to_decimal)
     df["EMA_20"] = df["Close"].ewm(span=20, adjust=False).mean().apply(format_to_decimal)
     df["STD_5"] = df["Close"].rolling(5).mean().apply(format_to_decimal)
@@ -33,51 +36,59 @@ def clean_and_add_features(df: pd.DataFrame, source: str):
 
     df["DailyReturn"] = df["Close"].pct_change().apply(format_to_decimal)
     df["Volatility"] = df["DailyReturn"].rolling(20).std().apply(format_to_decimal)
-    df["PriceChange"] = (df["Close"] - df["Open"]).apply(format_to_decimal)
- 
+    df["PriceChange"] = (df["Close"] - df["Open"]).apply(format_to_decimal) 
     return df
+
 
 def upload_silver_to_blob(df, serviceClient, containerName, blobName):
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=False)
+    df.to_csv(csv_buffer, index=False, encoding='utf-8')
     csv_buffer.seek(0)
 
     blob_client = serviceClient.get_blob_client(container=containerName, blob=blobName)
     blob_client.upload_blob(csv_buffer.getvalue(), overwrite=True)
 
+
 def append_latest_to_silver(df, serviceClient, containerName, blobName):
     try:
         blob_client = serviceClient.get_blob_client(container=containerName, blob=blobName)
         existing_data = blob_client.download_blob().readall().decode('utf-8')
-        existing_df = pd.read_csv(StringIO(existing_data))
+        existing_df = pd.read_csv(StringIO(existing_data), parse_dates=["Date"], keep_default_na=True)
+
         df = pd.concat([existing_df, df], ignore_index=True)
     except Exception as e:
         print(f"Error reading existing data: {e}")
 
     upload_silver_to_blob(df, serviceClient, containerName, blobName)
 
+
 def main():
     
     load_dotenv()
-    combined_df = pd.DataFrame()
+
+    combinedData = pd.DataFrame() # STAGING DATAFAME TO HANDLE FILES
     serviceClient = BlobServiceClient.from_connection_string(os.getenv("AZURE_CONNECTION_STRING"))
     blobContainer = serviceClient.get_container_client(os.getenv("CONTAINER_NAME"))
 
     bronzeLocation = os.getenv("BRONZE_LOCATION")
-    silverFileOutput = os.getenv("SILVER_OUTPUT")
+    silverFile = os.getenv("SILVER_LOCATION")
    
     for blob in blobContainer.list_blobs(name_starts_with=f"{bronzeLocation}"):
-        print(blob.name)
+        
         blob_client = blobContainer.get_blob_client(blob.name)
-        content = blob_client.download_blob().readall().decode('utf-8')
+        stockPrices = blob_client.download_blob().readall().decode('utf-8')
 
-        file = pd.read_csv(StringIO(content))
-        final = clean_and_add_features(file.head(1), os.path.basename(blob.name).replace('.csv',''))
+        data = pd.read_csv(StringIO(stockPrices), keep_default_na=True, parse_dates=["Date"])
+        
+        data = data.head(1)
+        data = clean_data(data)
+        data = add_features(data, os.path.basename(blob.name).replace('.csv',''))        
 
-        combined_df = pd.concat([combined_df, final], ignore_index=True)
-    upload_silver_to_blob(combined_df, serviceClient, os.getenv("CONTAINER_NAME"), f"{silverFileOutput}silver_output.csv")
-    #append_latest_to_silver(combined_df, serviceClient, os.getenv("CONTAINER_NAME"), f"{silverFileOutput}silver_output.csv")
-    return combined_df
+        combinedData = pd.concat([combinedData, data], ignore_index=True)
+    
+    #upload_silver_to_blob(combinedData, serviceClient, os.getenv("CONTAINER_NAME"), f"{silverFile}silver_output.csv")
+    append_latest_to_silver(combinedData, serviceClient, os.getenv("CONTAINER_NAME"), f"{silverFile}silver_output.csv")
+    return combinedData, data
 
 
 if __name__ == "__main__":
