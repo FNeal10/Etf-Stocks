c40aa31d30c4650813ad4fcaf42791324ddfd905
FNeal10 <altares.fneal@gmail.com>
2025-08-14 16:23:57 -0400

    modified env

--- api/main.py
+++ api/main.py
@@ -12,4 +12,5 @@ app = Flask(__name__)
 app.register_blueprint(api_routes)
 
 if __name__ == "__main__": 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
