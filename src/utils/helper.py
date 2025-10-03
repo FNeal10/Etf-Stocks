374c7ba8116713ee3f0bb405c2885554e80fec53
FNeal10 <altares.fneal@gmail.com>
2025-10-03 16:02:20 -0400

    added dbt

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,12 +1,6 @@
 import os
 from datetime import datetime
 
-
-#root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-#log_dir_path = os.path.join(root_dir, "logs")
-#filename = "log_" + datetime.now().strftime("%m%d%Y") + ".log"
-#log_file_path = os.path.join(log_dir_path, filename)
-
 def format_to_decimal(value):
     """
     Format a decimal value to two decimal places.
@@ -21,14 +15,3 @@ def format_to_decimal(value):
         return None
 
 
-# def check_or_create_logfile():
-#     os.makedirs(log_dir_path, exist_ok=True)    
-
-#     if not os.path.isfile(log_file_path):
-#         open(log_file_path, "w").close()
-
-# def append_to_log(message):
-#     check_or_create_logfile()
-    
-#     with open(log_file_path, "a") as log_file:
-#         log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
